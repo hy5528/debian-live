3fdcac262cb25de27b74bf4c3a75ba0e29698641
lukasblau <54887264+lukasblau@users.noreply.github.com>
2026-03-19 13:38:24 +0100

    feature: afdded dns capability to the live system

--- supportFiles/installChroot.sh
+++ supportFiles/installChroot.sh
@@ -20,6 +20,7 @@ update-locale LANG=en_US.UTF-8
 
 echo Install packages
 apt-get install -y --no-install-recommends linux-image-amd64 live-boot systemd-sysv
+apt-get install -y --no-install-recommends systemd-resolved
 apt-get install -y bash-completion cifs-utils curl dbus dosfstools firmware-linux-free gddrescue gdisk iputils-ping isc-dhcp-client less nfs-common ntfs-3g openssh-client open-vm-tools procps vim wimtools wget
 
 echo Clean apt post-install
@@ -27,6 +28,7 @@ apt-get clean
 
 echo Enable systemd-networkd as network manager
 systemctl enable systemd-networkd
+systemctl enable systemd-resolved
 
 echo Set resolv.conf to use systemd-resolved
 rm /etc/resolv.conf
